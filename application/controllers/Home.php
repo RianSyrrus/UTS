5a142f2819e1e31dc03f152b8ab8ee4e49ebf410
RianSyrrus <rian.syrrus01@gmail.com>
2026-05-18 20:08:36 +0700

    Update config

---
+++ application/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Membantu mengaktifkan fungsi base_url() yang kamu pakai di React
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        // Memanggil file codingan kamu yang ada di folder views
+        $this->load->view('Home');
+    }
+
+    public function katalog()
+    {
+        // Perhatikan huruf K kapital, harus sama dengan nama file di folder views
+        $this->load->view('Katalog');
+    }
+}
